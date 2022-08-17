782dda810613bb3fdc2fe37ac0d8e488178e3c7e
reyhanboydag <reyhanboydag@gmail.com>
2022-08-17 22:42:32 +0300

    index.js file added

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const axios = require("axios");
+const sha256context = require("sha256context");
+const https = require("https");
+const sslRedirect = require("heroku-ssl-redirect");
+
+// enable ssl redirect
+app.use(sslRedirect.default());
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/result", (req, res) => {
+  const { text } = req.query;
+  const sha256contextResult = sha256context(text);
+
+  res.render("result.ejs", { result: sha256contextResult });
+});
+
+app.listen(process.env.PORT || 3005, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
