3b7d61df5aad9989b151809f8d73157bbd8b043a
afarukcali <afarukcali@gmail.com>
2022-08-22 19:34:14 +0300

    some endpoints.

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const axios = require("axios");
-const sha256context = require("sha256context");
+const sha256streaming = require("@bitmatrix/sha256streaming");
 const https = require("https");
 const sslRedirect = require("heroku-ssl-redirect");
+const bodyParser = require("body-parser");
 
 // enable ssl redirect
 app.use(sslRedirect.default());
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -15,6 +22,12 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.post("/sha256initialize", (req, res) => {
+  const param1 = req.body.param1;
+  const sha256contextResult = sha256streaming.sha256Initializer(param1);
+  res.send(200, sha256contextResult);
 });
 
 app.get("/result", (req, res) => {
