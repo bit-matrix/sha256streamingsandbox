18a1cb6af0ddb54818369791fd734f58b753e600
afarukcali <afarukcali@gmail.com>
2022-08-25 00:30:24 +0300

    return.

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post("/sha256initialize", (req, res) => {
   const sha256contextResult = Sha256Streaming.sha256Initializer(param1.toString());
   console.log("sha256contextResultSuccess", sha256contextResult);
 
-  res.send(sha256contextResult);
+  res.status(200).json(sha256contextResult);
 });
 
 // app.get("/result", (req, res) => {
